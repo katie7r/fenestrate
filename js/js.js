08aed05a92f5e9430febe2cc75a17811a0cd5673
Katie Russ <katie.k7r@gmail.com>
2018-05-03 12:09:28 -0400

    Add first custom dimension frame

--- js/js.js
+++ js/js.js
@@ -2,26 +2,27 @@
 
   $(document).ready(frame);
 
+  $(window).on('hashchange', route);
+
   // TODO allow adding, removing, and refreshing frames
   $(document)
     .on('click', '.rotate', rotate)
+    .on('click', '.set-custom', setCustom)
     .on('click', '#set-src', setSrc);
 
-  $(window).on('hashchange', route);
-
-
   function frame() {
-    // TODO flexible frames
+    var $row = $('.devices.row');
+
+    // TODO custom frames
     var devices = [
+      { name: 'Tiny',         height: '480', width: '320' },
+      { name: '< XS',         height: '527', width: '320' },
       { name: 'iPhone 5/SE',  height: '568', width: '320' },
       { name: 'iPhone 6/7/8', height: '667', width: '375' },
       { name: 'iPhone X',     height: '812', width: '375' },
-      // { name: '', height: '', width: '' },
-      // { name: '', height: '', width: '' },
-      // { name: '', height: '', width: '' },
+      { name: 'iPad [?]',     height: '768', width: '1024' },
+      { name: 'Large',        height: '569', width: '767' },
     ];
-
-    var $row = $('.devices.row');
 
     $.each(devices, function(idx, device) {
       var $device, $heading, $iframe,
@@ -50,14 +51,40 @@
     $iframe.prop('width', height);
   }
 
-  function route() {
+  function route(event) {
+    // TODO stop anchor jump on active link click
+    event.stopPropagation();
+
     var target = (window.location.hash) ? window.location.hash : "#main";
     $('.page').hide();
     $(target).show();
-    console.log(target);
 
     $('.navbar-link').removeClass('active');
     $('.navbar-link[href="' + target + '"]').addClass('active');
+
+    window.scrollTo(0, 0);
+  }
+
+  function setCustom(event) {
+    event.stopPropagation();
+
+    var $device, $iframe, height, width;
+    $device = $(event.target).closest('.device');
+    $iframe = $device.find('iframe');
+    height  = $device.find('.custom-height').val();
+    width   = $device.find('.custom-width').val();
+
+    // TODO account for rotated or not in dimensions
+
+    if (!$iframe.prop('src')) {
+      $iframe.prop('src', $('#src').val());
+    }
+
+    $iframe
+      // .prop('src',    $('#src').val())
+      .prop('height', height)
+      .prop('width',  width)
+      .show();
   }
 
   function setSrc(event) {
